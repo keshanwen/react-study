9badbd56cbbd9969d1c9129dc6d679b0e360aee0
keshanwen <3640@winnermedical.com>
2024-03-13 16:16:34 +0800

    feat: React.creatElement 初态

---
+++ src/myReact/index.js
@@ -0,0 +1,9 @@
+import React from "./react";
+import ReactDOM from "./react-dom";
+let element1 = (
+  <div className="title" style={{ color: "red" }}>
+    <span>hello</span>world
+  </div>
+);
+console.log(JSON.stringify(element1, null, 2));
+ReactDOM.render(element1, document.getElementById("root"));
